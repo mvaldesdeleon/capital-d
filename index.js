807f331be7d1b3a51a1afc551df51c64be68726c
Martín Valdés de León <m.valdesdeleon@sovanta.com>
2017-05-10 23:06:17 +0200

    Shorthand schmorthand.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 module.exports = function D(t) {
     return new Proxy(typeof t === 'object' ? t : {}, {
-        get(t, p, r) {
+        get: function(t, p, r) {
             if (p === '_c_a_p_i_t_a_l__d_') return true;
             if (typeof t[p] === 'object' && !t[p]._c_a_p_i_t_a_l__d_) return t[p] = D(t[p]);
             else if (typeof t[p] === 'undefined') return t[p] = D();
